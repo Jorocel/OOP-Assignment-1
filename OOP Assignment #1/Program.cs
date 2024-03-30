0852a2af245eef12ba8c8fa4a9acceaf01404df3
Jorocel <jjorocel@gmail.com>
2024-03-30 12:06:14 +0800

    Initial commit

---
+++ OOP Assignment #1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace OOP_Assignment__1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int attempts = 3;
+
+            while (attempts > 0)
+            {
+                Console.Write("Input your username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Input your password: ");
+                string password = Console.ReadLine();
+
+                if (username == "Admin" && password == "Admin1234!")
+                {
+                    Console.WriteLine("Login succesful.");
+                    break;
+                }
+                else
+                {
+
+                    attempts--;
+                    if (attempts > 0)
+                        Console.WriteLine("Incorrect credentials. Please try again.");
+                    Console.WriteLine($"You have {attempts} attempts left.");
+                }
+            }
+            if (attempts == 0)
+            {
+                Console.WriteLine("You've used your maximum login attempts.Try again in 5 minutes.");
+            }
+        }
+    }
+}
